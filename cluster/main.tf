156ee53dc3c1fe6ed5106855b10ae6ce819eb440
John Sigvald Skauge <thejohnskauge@gmail.com>
2024-08-13 12:16:48 +0200

    Added aigw config that works

--- cluster/main.tf
+++ cluster/main.tf
@@ -47,65 +47,82 @@ resource "azurerm_public_ip" "ingress-ip" {
   location            = azurerm_resource_group.resource_group.location
   allocation_method   = "Static"
 
+  sku = "Standard" #Basic
+
     tags = merge(var.common_tags, var.tags)
 }
 
+# Need to create this since they are reused in multiple config settings in azurerm_application_gateway resource
+locals {
+  backend_address_pool_name      = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-beap"
+  frontend_port_name             = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-feport"
+  frontend_ip_configuration_name = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-feip"
+  http_setting_name              = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-be-htst"
+  listener_name                  = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-httplstn"
+  request_routing_rule_name      = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-rqrt"
+  redirect_configuration_name    = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-rdrcfg"
+}
 
-# resource azurerm_application_gateway ingress-agw {
-#   name                = "example-appgateway"
-#   resource_group_name = azurerm_resource_group.resource_group.name
-#   location            = azurerm_resource_group.resource_group.location
+resource azurerm_application_gateway ingress-gw {
+  name                = "${var.env}-cluster-ingress-gw"
+  resource_group_name = azurerm_resource_group.resource_group.name
 
-#   sku {
-#     name     = "Standard_v2"
-#     tier     = "Standard_v2"
-#     capacity = 2
-#   }
+  location            = azurerm_resource_group.resource_group.location
 
-#   gateway_ip_configuration {
-#     name      = "ingress-gateway-ip-configuration"
-#     subnet_id =  azurerm_subnet.cluster_subnet[2].id
-#   }
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
 
-#   frontend_port {
-#     name = "${var.env}-frontendport"
-#     port = 80
-#   }
+  gateway_ip_configuration {
+    name      = "ingress-gateway-ip-configuration"
+    subnet_id =  azurerm_subnet.cluster_subnet[2].id
+  }
 
-#   frontend_ip_configuration {
-#     name                 = azurerm_public_ip.ingress-ip.name
-#     public_ip_address_id = azurerm_public_ip.ingress-ip.id
-#   }
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
 
-#   backend_address_pool {
-#     name = azurerm_virtual_network.cluster_vpc.name
-#   }
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.ingress-ip.id
+  }
 
-#   backend_http_settings {
-#     name                  = "${var.env}-backend-htttp"
-#     cookie_based_affinity = "Disabled"
-#     path                  = "/"
-#     port                  = 80
-#     protocol              = "Http"
-#     request_timeout       = 60
-#   }
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
 
-#   http_listener {
-#     name                           = "${var.env}-http-listener"
-#     frontend_ip_configuration_name = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-frontend-ip"
-#     frontend_port_name             = "${var.env}-${azurerm_virtual_network.cluster_vpc.name}-frontendport-name"
-#     protocol                       = "Http"
-#   }
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 30
+  }
 
-#   request_routing_rule {
-#     name                       = "${var.env}-req-routing-rule"
-#     priority                   = 9
-#     rule_type                  = "Basic"
-#     http_listener_name         = "${var.env}-listener"
-#     backend_address_pool_name  = "${var.env}-backendpool"
-#     backend_http_settings_name = "${var.env}-backend-http-settings"
-#   }
-# }
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+
+  depends_on = [ azurerm_public_ip.ingress-ip, azurerm_resource_group.resource_group, azurerm_virtual_network.cluster_vpc, azurerm_subnet.cluster_subnet ]
+
+  tags = merge(var.common_tags, var.tags)
+}
 
 # other resources will go here
 resource "azurerm_kubernetes_cluster" "test-cluster" {
@@ -118,13 +135,15 @@ resource "azurerm_kubernetes_cluster" "test-cluster" {
 
     # Ingress application gateway needs its own subnet
     ingress_application_gateway {
-      gateway_name = "${var.env}-cluster-ingress-gw"
-      subnet_id = azurerm_subnet.cluster_subnet[2].id
-      # gateway_id = azurerm_application_gateway.ingress-agw.id
+      # gateway_name = "${var.env}-cluster-ingress-gw"
+      # subnet_id = azurerm_subnet.cluster_subnet[2].id
+      gateway_id = azurerm_application_gateway.ingress-gw.id
 
       
     }
 
+
+    sku_tier = "Free"
     
     
     identity {
@@ -161,7 +180,7 @@ resource "azurerm_kubernetes_cluster" "test-cluster" {
 
     automatic_channel_upgrade = "stable"
 
-    depends_on = [ azurerm_resource_group.resource_group, azurerm_subnet.cluster_subnet ]
+    depends_on = [ azurerm_resource_group.resource_group, azurerm_subnet.cluster_subnet, azurerm_application_gateway.ingress-gw, azurerm_public_ip.ingress-ip]
 }
 
 
@@ -172,11 +191,24 @@ resource "azurerm_role_assignment" "aks_agic_integration" {
 
 }
 
+## This might be unnecessary when agw have the same role on the vpc.
+resource "azurerm_role_assignment" "agw_integration" {
+  scope = azurerm_subnet.cluster_subnet[2].id
+  role_definition_name = "Network Contributor"
+  principal_id =  azurerm_kubernetes_cluster.test-cluster.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
+}
+
+
 resource "azurerm_role_assignment" "aci_connector_linux_integration" {
   scope = azurerm_virtual_network.cluster_vpc.id
   role_definition_name = "Network Contributor"
   principal_id =  azurerm_kubernetes_cluster.test-cluster.aci_connector_linux[0].connector_identity[0]["object_id"]
 }
+
+
+
+
+
 
 
 
